1935d8288b0ea85230704fd0e3bc7b588942ed18
Daniel Swanson <awesomeevildudes@gmail.com>
2025-04-22 20:45:47 -0400

    more deployment

--- AdvanSIS/settings.py
+++ AdvanSIS/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import secret
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v5_nwah8a%rgtx8tc#d$f4izzdm7ungz4b!qfdmbcdb9ve+sx9'
+SECRET_KEY = secret.SECRET_KEY
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = secret.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joseph.dangswan.com']
 
 
 # Application definition
