2d402b2a2ac568a5f902aea68f1f0ac3e797ec4d
Daniel Swanson <awesomeevildudes@gmail.com>
2025-09-08 17:48:19 -0400

    update query

--- app/views.py
+++ app/views.py
@@ -507,7 +507,10 @@ def course_resources(request, courseid):
             form = forms.AddFileForm()
     else:
         form = forms.AddFileForm()
-    qs = models.SharedFile.objects.filter(course=course).order_by('title')
+    qs = models.SharedFile.objects.filter(
+        Q(courses=course) | Q(templates=course.template) |
+        Q(objectives__in=course.template.learning_objectives.all())
+    ).order_by('title')
     if not request.user.is_staff:
         qs = qs.filter(Q(owner__isnull=True) | Q(owner=request.user.person))
     return render(request, 'app/course_resources.html', {
